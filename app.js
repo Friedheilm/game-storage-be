9962114560cb934d17911cc6c44bba6c41679331
Friedheilm <fridelok@gmail.com>
2023-11-16 22:46:35 +0100

    Adding local database in json file

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs'); // Node.js module for file system operations
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-// Simulated local database (replace with an actual database)
-const games = [
-  { id: 1, name: 'Game 1' },
-  { id: 2, name: 'Game 2' },
-  { id: 3, name: 'Game 3' },
-];
+const filePath = 'games.json'; // Path to JSON file that plays the role of local database
+
+// Read data from the JSON file
+function readDataFromFile() {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data from file:', error);
+    return [];
+  }
+}
+
+// Write data to the JSON file
+function writeDataToFile(data) {
+  try {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
+  } catch (error) {
+    console.error('Error writing data to file:', error);
+  }
+}
 
 // GET /games - Get all games
 app.get('/games', (req, res) => {
+  const games = readDataFromFile(); // Read games from the JSON file
   res.json(games);
 });
 
 // GET /games/:id - Get one game by ID
 app.get('/games/:id', (req, res) => {
+  const games = readDataFromFile(); // Read games from the JSON file
   const gameId = parseInt(req.params.id);
   const game = games.find((g) => g.id === gameId);
 
@@ -32,14 +50,19 @@ app.get('/games/:id', (req, res) => {
 
 // POST /games - Add a new game
 app.post('/games', (req, res) => {
+  const games = readDataFromFile(); // Read games from the JSON file
+  // Find the maximum ID in the existing games
+  const maxId = games.reduce((max, game) => (game.id > max ? game.id : max), 0);
   const newGame = req.body;
-  newGame.id = games.length + 1;
+  newGame.id = maxId + 1; // Generate a new ID
   games.push(newGame);
+  writeDataToFile(games);
   res.status(201).json(newGame);
 });
 
 // DELETE /games/:id - Delete a game by ID
 app.delete('/games/:id', (req, res) => {
+  const games = readDataFromFile(); // Read games from the JSON file
   const gameId = parseInt(req.params.id);
   const index = games.findIndex((g) => g.id === gameId);
 
@@ -47,6 +70,7 @@ app.delete('/games/:id', (req, res) => {
     res.status(404).json({ error: 'Game not found' });
   } else {
     games.splice(index, 1);
+    writeDataToFile(games); // Write updated games to the JSON file
     res.status(204).send();
   }
 });
